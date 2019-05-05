879e8ceb1b757f064fb7b8866efc04c263417c17
Johnbosco <boscoseries@gmail.com>
2019-05-05 11:01:40 +0100

    add test cases that will validate a string to currency method

---
+++ specs/toCurrency.test.js
@@ -0,0 +1,22 @@
+var String = require('../regex');
+
+describe("convert a number string to it currency equivalent", function() {
+
+  var numberString = '10123496.134';
+  var mixedString = '57rJK23496.1';
+  var nonString = 12345.656;
+
+  test("It should return a typeof of 'string'", function() {
+    expect(typeof numberString.toCurrency()).toBe('string');
+  });
+  test("It should return a string containing its inverse cases", function() {
+    expect(numberString.toCurrency()).toEqual(expect.stringContaining('10,123,496.134'));
+  });
+  test("It should throw if type of input is not a string", function() {
+    expect(function() { return nonString.toCurrency()}).toThrowError('not a function');
+  });
+  test("It should throw if input contains non-numbers", function() {
+    expect(function() { return mixedString.toCurrency()}).toThrow();
+  });
+
+});
