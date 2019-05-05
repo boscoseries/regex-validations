704f6f87890a1c10caf0928ed95f8e5e79f4931f
Johnbosco <boscoseries@gmail.com>
2019-05-05 11:02:23 +0100

    add method that converts a number string to its currency equivalent

--- regex.js
+++ regex.js
@@ -34,7 +34,7 @@ String.prototype.toLower = function () {
 String.prototype.ucFirst = function () {
   var regex = /\b([a-z])/g;
   for (string of this) {
-   var newString = this.replace(regex, function ($1) {
+    var newString = this.replace(regex, function ($1) {
       return $1.toUpper();
     })
   }
@@ -55,10 +55,28 @@ String.prototype.wordCount = function () {
   return this.word().length;
 }
 
-String.prototype.inverseCase = function() {
+String.prototype.toCurrency = function () {
+  var arrayOfCurrency = [];
+  var regex = /^(\d+).(\d+)$/;
+  var number = this.match(regex);
+  var wholeNumbers = number[1], decimalNumbers = number[2];
+
+  for (let index = wholeNumbers.length - 1; index >= 0; index--) {
+    var wholeNumber = wholeNumbers[index];
+    if (!arrayOfCurrency.length || arrayOfCurrency.length % 3 != 0) {
+      arrayOfCurrency.push(wholeNumber)
+    } else {
+      arrayOfCurrency.push(wholeNumber + ',');
+    }
+  }
+  var currencyToString = arrayOfCurrency.reverse().join('').concat('.' + decimalNumbers);
+  return currencyToString;
+}
+
+String.prototype.inverseCase = function () {
   var result = this.split(/\s/);
 
-  for (let index=0; index<this.length; index++) {
+  for (let index = 0; index < this.length; index++) {
     if (this[index].match(/[a-z]/g)) {
       result[index] = this[index].toUpper()
     } else {
@@ -68,15 +86,15 @@ String.prototype.inverseCase = function() {
   return result.join('');
 }
 
-String.prototype.alternatingCase = function() {
+String.prototype.alternatingCase = function () {
   var result = this.split('');
 
   if (result[0].match(/[a-z]/)) throw new Error('input should begin with an uppercase letter');
 
-  for (let index=0; index<this.length; index++) {
-      if (index%2==0) {
-        result[index] = this[index].toLower();
-      } else {
+  for (let index = 0; index < this.length; index++) {
+    if (index % 2 == 0) {
+      result[index] = this[index].toLower();
+    } else {
       result[index] = this[index].toUpper();
     }
   }
@@ -84,24 +102,22 @@ String.prototype.alternatingCase = function() {
 }
 
 String.prototype.numberWords = function () {
-  var numbersObject = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 0: 'zero'};
+  var numbersObject = { 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 0: 'zero' };
   var result = [];
   var regex = /\d/
-  
-  for (let index=0; index<this.length; index++) {
-    var wordNumber = this[index].replace(regex, function($1) {
+
+  for (let index = 0; index < this.length; index++) {
+    var wordNumber = this[index].replace(regex, function ($1) {
       return numbersObject[$1]
     })
     result.push(wordNumber);
   }
   return result.join(' ');
-  }
-  
+}
 
-String.prototype.isDigit = function() {
+String.prototype.isDigit = function () {
   regex = /^\d$/;
   return regex.test(this);
 }
-
 
 module.exports = String;
