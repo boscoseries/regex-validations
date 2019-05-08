6e8e5c021929466f17fb63e7b49f974b6ff9165e
Johnbosco <boscoseries@gmail.com>
2019-05-08 14:49:45 +0100

    implement feedback from instructor

--- specs/toCurrency.test.js
+++ specs/toCurrency.test.js
@@ -1,26 +1,32 @@
 var String = require('../regex');
 
-describe("convert a number string to it currency equivalent", function() {
-
+describe('convert a number string to it currency equivalent', function() {
   var numberString = '10123496.134';
   var numberString2 = '1689453';
   var mixedString = '57rJK23496.1';
   var nonString = 12345.656;
 
-  test("It should return a typeof of 'string'", function() {
+  test('It should return a typeof of string', function() {
     expect(typeof numberString.toCurrency()).toBe('string');
   });
-  test("It should return a currency representation of the number string", function() {
-    expect(numberString.toCurrency()).toEqual(expect.stringContaining('10,123,496.134'));
+  test('It should return a currency representation of the number string', function() {
+    expect(numberString.toCurrency()).toEqual(
+      expect.stringContaining('10,123,496.134')
+    );
   });
-  test("It should return a currency representation of the number string", function() {
-    expect(numberString2.toCurrency()).toEqual(expect.stringContaining('1,689,453'));
+  test('It should return a currency representation of the number string', function() {
+    expect(numberString2.toCurrency()).toEqual(
+      expect.stringContaining('1,689,453')
+    );
   });
-  test("It should throw if type of input is not a string", function() {
-    expect(function() { return nonString.toCurrency()}).toThrowError('not a function');
+  test('It should throw if type of input is not a string', function() {
+    expect(function() {
+      return nonString.toCurrency();
+    }).toThrowError('not a function');
   });
-  test("It should throw if input contains non-numbers", function() {
-    expect(function() { return mixedString.toCurrency()}).toThrow();
+  test('It should throw if input contains non-numbers', function() {
+    expect(function() {
+      return mixedString.toCurrency();
+    }).toThrow();
   });
-
 });
--- specs/toLower.test.js
+++ specs/toLower.test.js
@@ -1,18 +1,18 @@
 var String = require('../regex');
 
-describe("convert a string to lowercase", function() {
-
+describe('convert a string to lowercase', function() {
   var upperCaseString = 'ALL UPPERCASE LETTERS';
   var nonString = 12345;
 
-  test("It should return a typeof of String", function() {
+  test('It should return a typeof of String', function() {
     expect(typeof upperCaseString.toLower()).toBe('string');
   });
-  test("It should return a string with all lowercase letters ", function() {
+  test('It should return a string with all lowercase letters ', function() {
     expect(upperCaseString.toLower()).toEqual(expect.stringMatching(/[a-z]/));
   });
-  test("It should throw if type of input is not a string", function() {
-    expect(function() { return nonString.toLower()}).toThrow();
+  test('It should throw if type of input is not a string', function() {
+    expect(function() {
+      return nonString.toLower();
+    }).toThrow();
   });
-
 });
